e04ab1238c7cf27a03a0e83a133fe97ce82f7343
fabrizio_pimentel <fabriziopimentel@ciencias.unam.mx>
2026-03-26 11:46:41 -0600

    el 3, osea el 1 de la parte 2 hecho nyajajajaa

--- tres.hs
+++ tres.hs
@@ -1,3 +1,6 @@
+import Prelude hiding (Eq, Ord, minimum)
+
+
 obtenerSaldo :: Int -> Maybe Int
 obtenerSaldo = undefined
 
@@ -19,3 +22,47 @@ sumarSaldos = \mx my ->
     mx >>= \x ->
     my >>= \y ->
     return (x + y)
+
+
+
+--record de eqDict
+data EqDict a = EqDict
+  { eq :: a -> a -> Bool
+  }
+
+--record de ordict
+data OrdDict a = OrdDict
+  { leq    :: a -> a -> Bool
+  , eqDict :: EqDict a
+  }
+
+eqIntDict :: EqDict Int
+eqIntDict = EqDict
+  { eq = \x y -> x == y
+  }
+
+ordIntDict :: OrdDict Int
+ordIntDict = OrdDict
+  { leq = \x y -> x <= y
+  , eqDict = eqIntDict
+  }
+
+-- Devuelve el minimo de una lista no vacia
+minimum :: OrdDict a -> [a] -> a
+minimum = \dict xs ->
+  case xs of
+    [x] -> x
+    (x:xs') ->
+      let m = minimum dict xs'
+      in if leq dict m x then m else x
+
+
+
+-- Verifica si una lista esta ordenada
+estaOrdenada :: OrdDict a -> [a] -> Bool
+estaOrdenada = \dict xs ->
+  case xs of
+    [] -> True
+    [_] -> True
+    (x:y:ys) ->
+      leq dict x y && estaOrdenada dict (y:ys)
